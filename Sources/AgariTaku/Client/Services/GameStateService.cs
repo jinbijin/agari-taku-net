0c6688cd189137f67aaf1a4f2e374394f6126479
Jinbi Jin <jinbijin@jinbijin.nl>
2020-11-08 20:53:32 +0000

    Add client sync code

---
+++ Sources/AgariTaku/Client/Services/GameStateService.cs
@@ -0,0 +1,42 @@
+﻿using AgariTaku.Shared.Messages;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AgariTaku.Client.Services
+{
+    public class GameStateService
+    {
+        private HubConnection? _connection;
+        private readonly Dictionary<int, Stopwatch> _stopwatches = new();
+        private readonly Dictionary<int, long> _delays = new();
+
+        public async Task StartConnection()
+        {
+            _connection = new HubConnectionBuilder().WithUrl($"https://localhost:5001/gamehub").Build();
+
+            _connection.On<SyncTickMessage>("ServerSyncTick", ServerSyncTick);
+            _connection.On<SyncTickMessage>("AckSyncTick", AckSyncTick);
+
+            await _connection.StartAsync();
+        }
+
+        public void ServerSyncTick(SyncTickMessage message)
+        {
+            _connection.InvokeAsync<SyncTickMessage>("ClientSyncTick", message);
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            _stopwatches.Add(message.TickNumber, stopwatch);
+        }
+
+        public void AckSyncTick(SyncTickMessage message)
+        {
+            Stopwatch stopwatch = _stopwatches[message.TickNumber];
+            stopwatch.Stop();
+            _delays.Add(message.TickNumber, stopwatch.ElapsedMilliseconds);
+        }
+
+        public IDictionary<int, long> Delays => _delays;
+    }
+}
