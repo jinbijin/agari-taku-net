4f878936e14fbb64446ec82f165e156721beedca
Jinbi Jin <jinbijin@jinbijin.nl>
2020-11-08 15:34:45 +0000

    Add SignalR

--- Sources/AgariTaku/Server/Startup.cs
+++ Sources/AgariTaku/Server/Startup.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Linq;
 
 namespace AgariTaku.Server
 {
@@ -18,16 +15,13 @@ namespace AgariTaku.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSignalR().AddMessagePackProtocol();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -52,6 +46,7 @@ namespace AgariTaku.Server
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                // Add hubs here...
                 endpoints.MapFallbackToFile("index.html");
             });
         }
