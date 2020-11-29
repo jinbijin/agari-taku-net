f2f161a91faba1c63b945d8775d3f7cb8f33ea29
Jinbi Jin <jinbijin@jinbijin.nl>
2020-11-29 14:46:43 +0000

    Add SendAccumulatedTicks tests

--- Sources/AgariTaku.Tests/Client/TickServiceTests.cs
+++ Sources/AgariTaku.Tests/Client/TickServiceTests.cs
@@ -1,9 +1,12 @@
-﻿using AgariTaku.Client.Services;
+﻿using AgariTaku.Client.HubClients;
+using AgariTaku.Client.Services;
 using AgariTaku.Shared.Common;
+using AgariTaku.Shared.Messages;
 using AgariTaku.Shared.Types;
 using AgariTaku.Tests.Common;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using Moq;
 using System;
 using System.Linq;
 using Xunit;
@@ -14,12 +17,14 @@ namespace AgariTaku.Tests.Client
     {
         private readonly ClientState _clientState;
         private readonly TickService _tickService;
+        private readonly Mock<IGameHubClient> _clientMock;
 
         public TickServiceTests()
         {
             IConfiguration configuration = new TestConfiguration();
             _clientState = new ClientState(configuration);
             _tickService = new TickService(configuration, _clientState);
+            _clientMock = new Mock<IGameHubClient>();
         }
 
         [Fact]
@@ -90,6 +95,70 @@ namespace AgariTaku.Tests.Client
 
             // Assert
             _clientState.ClientTickBuffer.Slice(0, 5).Select(tick => tick?.TickNumber).Should().Equal(10, 11, 12, 8, 9);
+        }
+
+        [Fact]
+        public void SendAccumulatedTicksShouldSendBufferSizeNonAckedTicks()
+        {
+            // Arrange
+            using AssertionScope scope = new();
+
+            int ackTickNumber = 8;
+            _clientMock.Setup(m => m.ClientGameTick(It.IsAny<ClientGameTickMessage>()))
+                .Callback<ClientGameTickMessage>(message =>
+                {
+                    message.AckTick.Should().Equal(10, ackTickNumber, 10, 10, 11);
+                });
+
+            int currentTickNumber = 12;
+            _clientState.AckTicks[TickSource.Server] = 10;
+            _clientState.AckTicks[TickSource.East] = ackTickNumber;
+            _clientState.AckTicks[TickSource.South] = 10;
+            _clientState.AckTicks[TickSource.West] = 10;
+            _clientState.AckTicks[TickSource.North] = 11;
+            _clientState.CurrentTick = currentTickNumber;
+            for (int i = ackTickNumber + 1; i <= currentTickNumber; i++)
+            {
+                _clientState.ClientTickBuffer[i] = new() { TickNumber = i };
+            }
+
+            // Act
+            _tickService.SendAccumulatedTicks(_clientMock.Object);
+
+            // Assert
+            _clientMock.Verify(m => m.ClientGameTick(It.IsAny<ClientGameTickMessage>()), Times.Once);
+        }
+
+        [Fact]
+        public void SendAccumulatedTicksShouldSendAckedTicks()
+        {
+            // Arrange
+            using AssertionScope scope = new();
+
+            int ackTickNumber = 8;
+            _clientMock.Setup(m => m.ClientGameTick(It.IsAny<ClientGameTickMessage>()))
+                .Callback<ClientGameTickMessage>(message =>
+                {
+                    message.AckTick.Should().Equal(10, ackTickNumber, 10, 10, 11);
+                });
+
+            int currentTickNumber = 12;
+            _clientState.AckTicks[TickSource.Server] = 10;
+            _clientState.AckTicks[TickSource.East] = ackTickNumber;
+            _clientState.AckTicks[TickSource.South] = 10;
+            _clientState.AckTicks[TickSource.West] = 10;
+            _clientState.AckTicks[TickSource.North] = 11;
+            _clientState.CurrentTick = currentTickNumber;
+            for (int i = ackTickNumber + 1; i <= currentTickNumber; i++)
+            {
+                _clientState.ClientTickBuffer[i] = new() { TickNumber = i };
+            }
+
+            // Act
+            _tickService.SendAccumulatedTicks(_clientMock.Object);
+
+            // Assert
+            _clientMock.Verify(m => m.ClientGameTick(It.IsAny<ClientGameTickMessage>()), Times.Once);
         }
     }
 }
